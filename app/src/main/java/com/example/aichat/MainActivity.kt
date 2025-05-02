903e42ea47d99d15f290c1a01b0978b717ffda1b
NataliaAlv <natalia.alvarez@secture.com>
2025-05-02 12:20:54 +0200

    Updated Hilt annotation

--- app/src/main/java/com/example/aichat/MainActivity.kt
+++ app/src/main/java/com/example/aichat/MainActivity.kt
@@ -6,8 +6,10 @@ import androidx.activity.compose.setContent
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import com.example.aichat.ui.screens.chat.ChatScreen
+import dagger.hilt.android.AndroidEntryPoint
 import values.theme.AIChatTheme
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
